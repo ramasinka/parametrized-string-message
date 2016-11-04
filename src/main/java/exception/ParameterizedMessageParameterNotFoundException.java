d93941eb50a6d7a177556cd9ea7650dbbc010eff
Ramasinka <Romas.noreika@yahoo.com>
2016-11-04 12:44:53 +0200

    Parametrized-string-message builder pattern

---
+++ src/main/java/exception/ParameterizedMessageParameterNotFoundException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class ParameterizedMessageParameterNotFoundException extends Throwable {
+    public ParameterizedMessageParameterNotFoundException(String message) {
+        super(message);
+    }
+}
+
