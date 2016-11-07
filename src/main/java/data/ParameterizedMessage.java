4626b67a39b1d7464344f13157131220661831be
Ramasinka <Romas.noreika@yahoo.com>
2016-11-07 22:25:20 +0200

    Parametrized message

--- src/main/java/data/ParameterizedMessage.java
+++ src/main/java/data/ParameterizedMessage.java
@@ -1,20 +1,46 @@
 package data;
 
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ParameterizedMessage {
 
-    private String user;
-    private String name;
-    private String password;
+    Map<String, String> parametersMap = new HashMap<String, String>();
 
-    public ParameterizedMessage(String user, String name, String password) {
-        this.user = user;
-        this.name = name;
-        this.password = password;
-
+    public ParameterizedMessage(Map<String, String> parametersMap) {
+        this.parametersMap = parametersMap;
     }
 
-    public String replaceParametrizedMessage(String parametrizedMessage) {
-        parametrizedMessage = parametrizedMessage.replace("${user}", user).replace("${username}", name).replace("${password}", password);
-        return parametrizedMessage;
+    public String replaceParametrizedMessage(String message, Map<String, String> parametersMap) {
+        String resolvedString = null;
+        this.parametersMap = parametersMap;
+        StrSubstitutor sub = new StrSubstitutor(parametersMap);
+        if (checkIfMessageHaveAllParameters(parametersMap, message)) {
+            resolvedString = sub.replace(message);
+        }
+        return resolvedString;
+    }
+
+    public boolean checkIfMessageHaveAllParameters(Map<String, String> parametersMap, String message) {
+        this.parametersMap = parametersMap;
+        Iterator entries = parametersMap.entrySet().iterator();
+        while (entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = (String) entry.getKey();
+            if (findParameterInTheMessage(key, message) >= 0) {
+                continue;
+            } else {
+                throw new IllegalArgumentException("Your message don't have parameter with name:" + key);
+            }
+        }
+        return true;
+    }
+
+    private int findParameterInTheMessage(String parameter, String message) {
+        return message.indexOf(parameter);
     }
 }
--- src/test/java/data/ParameterizedMessageTest.java
+++ src/test/java/data/ParameterizedMessageTest.java
@@ -2,24 +2,35 @@ package data;
 
 import org.junit.Before;
 import org.junit.Test;
-import service.ParametrizedMessageService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class ParameterizedMessageTest {
-    private ParameterizedMessage parameterizedMessage;
-    private ParametrizedMessageService parametrizedMessageService;
-
-    @Before
-    public void setUp() throws Exception {
-        parameterizedMessage = new ParameterizedMessage("jonas", "john", "abs123");
-        parametrizedMessageService = new ParametrizedMessageService();
-    }
 
     @Test
     public void parametrizedStringReplace() {
-        String parametrizedMessage = "Hello ${user}, your username is ${username} and password ${password}";
+        Map paramsMap = new HashMap();
+        paramsMap.put("user", "jonas");
+        paramsMap.put("username", "john");
+        paramsMap.put("password", "abs123");
+        ParameterizedMessage parameterizedMessage = new ParameterizedMessage(paramsMap);
+        String message = "Hello ${user}, your username is ${username} and password ${password}";
         String replacedParametrizedMessage = "Hello jonas, your username is john and password abs123";
-        assertEquals(replacedParametrizedMessage, parameterizedMessage.replaceParametrizedMessage(parametrizedMessage));
+        assertEquals(replacedParametrizedMessage, parameterizedMessage.replaceParametrizedMessage(message, paramsMap));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotReplaceParametrizedMessageWithoutParameter() {
+        String message = "Hello ${user}, your username is ${username} and password ${password}";
+        Map paramsMap = new HashMap();
+        paramsMap.put("user", "jonas");
+        paramsMap.put("username", "john");
+        paramsMap.put("password", "abs123");
+        paramsMap.put("email", "abs123@gmail.com");
+        ParameterizedMessage parameterizedMessage = new ParameterizedMessage(paramsMap);
+        parameterizedMessage.replaceParametrizedMessage(message, paramsMap);
     }
 }
