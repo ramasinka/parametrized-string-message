f600e3fe07f62e5e50ed3fe850721b65d85e9cc9
Ramasinka <Romas.noreika@yahoo.com>
2016-11-09 13:50:55 +0200

    Parametrized message

--- src/main/java/data/ParameterizedMessage.java
+++ src/main/java/data/ParameterizedMessage.java
@@ -45,7 +45,7 @@ public class ParameterizedMessage {
             String parameterName = entry.getKey();
             String parameterValue = entry.getValue();
             if (findParameterInTheMessage(parameterName) < 0) {
-                log.warn("In your message: " + message + " parameter name: " + parameterName + " with value: " + parameterValue + " not used");
+                log.warn("In your message: " + message + " parameter with name: " + parameterName + " and value: " + parameterValue + " not used");
             }
         }
     }
